b91192fb041065bfc42656c81476dd33226fc680
Disasterdon <disasterdon@outlook.com>
2024-12-11 13:00:19 +0100

    ob

--- configurator.php
+++ configurator.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vranic Company Limited || Configurator TEST</title>
+    <title>VCL | Configurator</title>
     <!-- custom css-->
     <link rel="stylesheet" href="/css/style.css" />
     <!-- font awesome-->
